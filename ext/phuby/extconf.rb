80d3d99126eac673ea9d8822b1fbbe8a266fea64
Aaron Patterson <aaron.patterson@gmail.com>
2016-05-02 10:08:18 -0700

    this seems to be working

--- ext/phuby/extconf.rb
+++ ext/phuby/extconf.rb
@@ -5,36 +5,23 @@ ENV['RC_ARCHS'] = '' if RUBY_PLATFORM =~ /darwin/
 require 'mkmf'
 require 'shellwords'
 
-location = File.expand_path(File.dirname(__FILE__))
+PREFIX = File.expand_path(File.dirname(__FILE__))
+def build_php
+  location = PREFIX
 
-config = Dir["/{usr,opt}/local/bin/php-config"].first
-prefix = `#{config} --prefix`.chomp if config
-
-if prefix
-  php_inc, php_lib = dir_config("php5", "#{prefix}/include", "#{prefix}/lib")
-else
-  php_inc, php_lib = dir_config("php5")
-end
-
-$INCFLAGS = "-I#{File.join(php_inc, "php").quote} #{$INCFLAGS}"
-
-%w{ Zend TSRM main }.each do |dir|
-  $INCFLAGS = "-I#{File.join(php_inc, "php", dir).quote} #{$INCFLAGS}"
-end
-
-unless find_library("php5", "php_embed_init", php_lib)
   require 'tmpdir'
   # This really only works on OS X, and I don't want to figure out other
   # operating systems.  If you're not on OS X, just install PHP youself.
   Dir.chdir Dir.tmpdir do
 
-    unless File.file? 'php-5.6.9.tar.gz'
-      system "curl -O http://php.net/distributions/php-5.6.9.tar.gz"
+    unless File.file? 'php-5.6.21.tar.gz'
+      system "curl -O http://php.net/distributions/php-5.6.21.tar.gz"
     end
 
-    system "tar zxf php-5.6.9.tar.gz"
-    Dir.chdir "php-5.6.9" do
-      system "curl https://raw.githubusercontent.com/tenderlove/phuby/ed41daece6a9191a0240dae9c9b610bb06d61177/configure.diff | patch"
+    system "tar zxf php-5.6.21.tar.gz"
+    Dir.chdir "php-5.6.21" do
+      #system "curl https://raw.githubusercontent.com/tenderlove/phuby/ed41daece6a9191a0240dae9c9b610bb06d61177/configure.diff | patch"
+      system "curl https://gist.githubusercontent.com/tenderlove/e3fd03049c687f86c2f3236f35dfb080/raw/68ada4c25188a57da120cadb0ee16d18967404f9/fuu.diff | patch"
       cmd = './configure --enable-debug --enable-embed --disable-cli --with-mysql=/usr/local --with-mysqli=/usr/local/bin/mysql_config --with-mysql-sock=/tmp/mysql.sock '
       cmd << " --prefix=#{Shellwords.escape(location)}"
       system cmd
@@ -55,7 +42,39 @@ unless find_library("php5", "php_embed_init", php_lib)
       system "make install"
     end
   end
-  php_inc, php_lib = dir_config("php5", "#{location}/include", "#{location}/lib")
+end
+
+def find_config
+  [
+    "/usr/local/bin/php-config",
+    "/opt/local/bin/php-config",
+    File.join(PREFIX, 'bin', 'php-config')
+  ].find { |f| File.exist? f }
+end
+
+config = find_config
+
+if config
+else
+  build_php
+  config = find_config
+end
+
+prefix = `#{config} --prefix`.chomp if config
+
+if prefix
+  php_inc, php_lib = dir_config("php5", "#{prefix}/include", "#{prefix}/lib")
+else
+  php_inc, php_lib = dir_config("php5")
+end
+
+$INCFLAGS = "-I#{File.join(php_inc, "php").quote} #{$INCFLAGS}"
+
+%w{ Zend TSRM main }.each do |dir|
+  $INCFLAGS = "-I#{File.join(php_inc, "php", dir).quote} #{$INCFLAGS}"
+end
+
+unless find_library("php5", "php_embed_init", php_lib)
 
   unless find_library("php5", "php_embed_init", php_lib)
     abort "php is missing!"
